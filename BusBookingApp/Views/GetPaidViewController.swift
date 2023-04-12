c7d1dafb214c1121dbd3920526188f9a92f9dfcc
Baki ucan <bakiucann@gmail.com>
2023-04-12 18:29:09 +0300

    final project uploaded without readme

--- BusBookingApp/Views/GetPaidViewController.swift
+++ BusBookingApp/Views/GetPaidViewController.swift
@@ -40,10 +40,54 @@ class GetPaidViewController: UIViewController {
             }
   
     @IBAction func getPaidOnayla(_ sender: Any) {
-        Passenger.name = name.text
-        Passenger.surname = surname.text
-        Passenger.Id = id.text
+        // Ad, soyad ve ID alanlarının boş olup olmadığını kontrol edin.
+        guard let name = name.text, !name.isEmpty else {
+            showAlert(message: "Lütfen adınızı girin.")
+            return
+        }
+        guard let surname = surname.text, !surname.isEmpty else {
+            showAlert(message: "Lütfen soyadınızı girin.")
+            return
+        }
+        guard let id = id.text, !id.isEmpty else {
+            showAlert(message: "Lütfen kimlik numaranızı girin.")
+            return
+        }
+        
+        // Kredi kartı bilgileri alanlarının boş olup olmadığını kontrol edin.
+        guard let ccName = nameSurname.text, !ccName.isEmpty else {
+            showAlert(message: "Lütfen kredi kartı üzerindeki ismi girin.")
+            return
+        }
+        guard let ccNumber = ccNumber.text, !ccNumber.isEmpty else {
+            showAlert(message: "Lütfen kredi kartı numarasını girin.")
+            return
+        }
+        guard let cvv = CVV.text, !cvv.isEmpty else {
+            showAlert(message: "Lütfen CVV kodunu girin.")
+            return
+        }
+        guard let lastDate = lastDate.text, !lastDate.isEmpty else {
+            showAlert(message: "Lütfen son kullanma tarihini girin.")
+            return
+        }
+
+        // Tüm alanlar doluysa yolcu bilgilerini kaydedin.
+        Passenger.name = name
+        Passenger.surname = surname
+        Passenger.Id = id
+        
+        // Ödeme onay sayfasına git.
+        performSegue(withIdentifier: "paymentConfirmed", sender: nil)
     }
+
+    private func showAlert(message: String) {
+        let alertController = UIAlertController(title: "Uyarı", message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Tamam", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
             if segue.identifier == "paymentConfirmed",
                let paymentConfirmedVC = segue.destination as? PaymentConfirmedViewController {
